71d46ce085c906cbedd53bc787d3c90c05c66195
MengXJ <907564255@qq.com>
2017-01-13 13:05:18 +0800

    delete

--- controller/fbController.js
+++
@@ -1,6 +0,0 @@
-var app = angular.module('fbApp',[]);
-app.controller('fbCtrl',function($scope){
-	$scope.urltop = 'resources/nav.html';
-	$scope.urlcotop = 'resources/nav_copartner.html';
-	$scope.urlfooter = 'resources/footer.html';
-})
